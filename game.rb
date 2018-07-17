cd3ba77741592bd6addb86a91d964186572f9ffa
Mario <mario.antoniali@gmail.com>
2018-07-17 21:30:17 +0200

    modifs

--- game.rb
+++ game.rb
@@ -1,153 +1,113 @@
 require_relative "board"
 require_relative "player"
 
-class Game 
-
-  attr_accessor :ending, :turn
-
-  def initialize(rules)
-    @ending = false                 # initialise la fin du jeu
-    @turn = Random.rand(0..1)       # choisit le premier joueur 
-    @players = []                   # initialise et récupère les noms des joueurs
-    puts " Veuillez entrer le nom du 1er joueur ( o ) "
-    name = gets.chomp 
-    @players.push(addPlayer(name,"o"))
-    puts " Veuillez entrer le nom du 2eme joueur ( x )"
-    name = gets.chomp 
-    @players.push(addPlayer(name,"x"))
-    @@cases = []
-  end
-
-  def altern                        # définit l'alternance entre les joueurs tour par tour
-    if @turn == 0
-      @turn += 1
-      else @turn -= 1
+class Game
+    
+    attr_accessor :ending, :turn
+    
+    def initialize(rules)
+        @ending = false                 # initialise la fin du jeu
+        @turn = Random.rand(0..1)       # choisit le premier joueur
+        @players = []                   # initialise et récupère les noms des joueurs
+        puts " Veuillez entrer le nom du 1er joueur ( o ) :"
+        name = gets.chomp.capitalize
+        @players.push(add_player(name,"o"))
+        puts " Veuillez entrer le nom du 2ème joueur ( x ) :"
+        name = gets.chomp.capitalize
+        @players.push(add_player(name,"x"))
     end
-  end
-
-  def action                        
-    @board = Board.new
-    begin
     
-    puts "Bienvenue sur le TicTacToe lyonnais !"
-    puts "Tape 1 pour jouer"
-    puts "Tape 2 pour afficher les règles"
-    puts "Tape 3 pour afficher les cases"
-    puts "Tape 4 pour sortir du jeu"
-
-    i = gets.chomp.to_i
-    match_nul = false
-    
-    if    @@cases[0] == "x" && @@cases[1] == "x" && @@cases[2] == "x" || #verifie une victoire Horizontale pour
-          @@cases[3] == "x" && @@cases[4] == "x" && @@cases[5] == "x" ||
-          @@cases[6] == "x" && @@cases[7] == "x" && @@cases[8] == "x" ||
-    
-          @@cases[0] == "x" && @@cases[3] == "x" && @@cases[6] == "x" || #verfier une victoire verticale pour
-          @@cases[1] == "x" && @@cases[4] == "x" && @@cases[7] == "x" ||
-          @@cases[2] == "x" && @@cases[5] == "x" && @@cases[8] == "x" ||
-    
-          @@cases[0] == "x" && @@cases[4] == "x" && @@cases[8] == "x" || #verifier una victoire diagonale
-          @@cases[2] == "x" && @@cases[4] == "x" && @@cases[6] == "x"
-          return victory1 
-    
-    elsif @@cases[0] == "o" && @@cases[1] == "o" && @@cases[2] == "o" ||
-          @@cases[3] == "o" && @@cases[4] == "o" && @@cases[5] == "o" ||
-          @@cases[6] == "o" && @@cases[7] == "o" && @@cases[8] == "o" ||
-    
-          @@cases[0] == "o" && @@cases[3] == "o" && @@cases[6] == "o" ||
-          @@cases[1] == "o" && @@cases[4] == "o" && @@cases[7] == "o" ||
-          @@cases[2] == "o" && @@cases[5] == "o" && @@cases[8] == "o" ||
-    
-          @@cases[0] == "o" && @@cases[4] == "o" && @@cases[8] == "o" ||
-          @@cases[2] == "o" && @@cases[4] == "o" && @@cases[6] == "o"
-          return victory2
-          else 
-          return match_nul
-          end
-
-    if i == 1
-
-      while result == match_nul  do
-
-        puts "#{@players[@turn].player_name}, choisis une case."
-        cases = gets.chomp.to_i
-        while cases < 1 || cases > 9
-          puts"Merci d'entrer un nombre entre 1 et 9"
-          print ">"
-          cases = gets.chomp.to_i
+    # définit l'alternance entre les joueurs tour par tour
+    def altern                       
+        if @turn == 0
+            @turn += 1
+            else @turn -= 1
         end
-        puts "#{@players[@turn].player_name} a choisi la case #{cases}"
-        no_problem = @board.update_case(cases.to_i - 1, @players[@turn].symbol)  # update le fichier board avec les valeurs des cases
+    end
+    
+    # lance le jeu après avoir créé les 2 joueurs
+    def action
+        @board = Board.new # Créé le tableau
+        begin
+            
+            puts "Bienvenue sur le TicTacToe lyonnais !"
+            puts "Tape 1 pour jouer"
+            puts "Tape 2 pour afficher les règles"
+            puts "Tape 3 pour afficher les cases"
+            puts "Tape 4 pour sortir du jeu"
+            
+            i = gets.chomp.to_i
+            
+            continue = true #definition variable permettant de continuer le jeu tant que continue = true
+            
+            if i == 1
+                begin 
+                    
+                    puts "#{@players[@turn].player_name}, choisis une case." #annonce le joueur qui doit jouer en fonction du tour
+                    cases = gets.chomp.to_i 
+                    while cases < 1 || cases > 9 #le joueur doit choisir un nbre entre 1 et 9 sinon on renvoit le message en dessous
+                        puts"Merci d'entrer un nombre entre 1 et 9"
+                        print ">"
+                        cases = gets.chomp.to_i
+                    end
+                    puts "#{@players[@turn].player_name} a choisi la case #{cases}" 
+                    no_problem = @board.update_case(cases.to_i - 1, @players[@turn].symbol)  # verifie si la case est disponible
+                    
+                    @board.display #affiche le tableau apres chaque tour
+                    
+                    if @board.game_ending #determine le gagnant potentiel
+                                        
+                        if(@board.get_symbole_winner == @players[0].symbol)
+                            puts "#{@players[0].player_name} a gagné"
+                        elsif(@board.get_symbole_winner == @players[1].symbol)
+                            puts "#{@players[0].player_name} a gagné"
+                            else
+                            puts " Match teminé pas de gagnant"
+                        
+                        end
+                        
+                        continue = false
+                        @board = Board.new # réinitialise un nouveau tableau si partie terminee
+
+                    break
+                    else
+                        
+                        if no_problem == 1 #la case est disponible et le jeu est pas encore termine
+                        altern
+                    else
+                        puts "Cette case est déjà prise, essaie encore !"
+                        
+                    end
+                        
+                    end    
+                    
+                    
+                    
+                end while(continue) #fin de begin
+                
+            elsif i == 2
+            puts "Il y a deux joueurs; l’un joue avec le signe x et l’autre avec le signe o. Les deux joueurs remplissent alternativement les cellules vides. Au début du jeu, il y a 9 cellules vides dans un panneau 3x3. Le but est de placer 3 signes identiques sur une colonne, une ligne ou une diagonale. Le jeu prend fin si le panneau est entièrement rempli et si aucun joueur n’arrive à atteindre le but. Bonne chance !\n\n"
+                
+            elsif i == 3
+            @board.show_index            # affiche le tableau avec le nom des cases
+            puts "\n"
+                
+            elsif i == 4
+                
+            else
+            puts "Mauvaise commande, recommence !"
+                
+            end
+        end while i != 4
         
-        @board.display
-
-        if no_problem == 1
-        altern
-        else
-        puts "Cette case est déjà prise, essaie encore !"
-        end
-      end
-      while result == victory1 
-        puts "x a gagné"
-      end
-      while result == victory2
-        puts "o a gagné"
-      end
-
-    elsif i == 2
-      puts "Il y a deux joueurs; l’un joue avec le signe x et l’autre avec le signe o. Les deux joueurs remplissent alternativement les cellules vides. Au début du jeu, il y a 9 cellules vides dans un panneau 3x3. Le but est de placer 3 signes identiques sur une colonne, une ligne ou une diagonale. Le jeu prend fin si le panneau est entièrement rempli et si aucun joueur n’arrive à atteindre le but. Bonne chance !\n\n"
+    end
     
-    elsif i == 3
-      @board.show_index            # affiche le tableau avec le nom des cases
-      puts "\n"
+    def add_player(name,symbol)       # définit le joueur et son symbole
+        player = Player.new(name,symbol)
+        return player
+    end
     
-    elsif i == 4
-    
-    else 
-      puts "Mauvaise commande, recommence !"
-
-    end 
-  end while i != 4
-
-  end
-
-  # def game_ending
-	# 	# verifier si joueur avec symbol X a gagne
-  #     if @cases[0] == "x" && @cases[1] == "x" && @cases[2] == "x" || #verifie une victoire Horizontale pour
-  #  		 @cases[3] == "x" && @cases[4] == "x" && @cases[5] == "x" ||
-  #  		 @cases[6] == "x" && @cases[7] == "x" && @cases[8] == "x" ||
-
-  #  		 @cases[0] == "x" && @cases[3] == "x" && @cases[6] == "x" || #verfier une victoire verticale pour
-  #      @cases[1] == "x" && @cases[4] == "x" && @cases[7] == "x" ||
-  #      @cases[2] == "x" && @cases[5] == "x" && @cases[8] == "x" ||
-
-  #      @cases[0] == "x" && @cases[4] == "x" && @cases[8] == "x" || #verifier una victoire diagonale
-  #      @cases[2] == "x" && @cases[4] == "x" && @cases[6] == "x"
-  #     puts "X a gagné"  
-
-  #     elsif @cases[0] == "o" && @cases[1] == "o" && @cases[2] == "o" ||
-  #      @cases[3] == "o" && @cases[4] == "o" && @cases[5] == "o" ||
-  #      @cases[6] == "o" && @cases[7] == "o" && @cases[8] == "o" ||
-
-  #      @cases[0] == "o" && @cases[3] == "o" && @cases[6] == "o" ||
-  #      @cases[1] == "o" && @cases[4] == "o" && @cases[7] == "o" ||
-  #      @cases[2] == "o" && @cases[5] == "o" && @cases[8] == "o" ||
-
-  #      @cases[0] == "o" && @cases[4] == "o" && @cases[8] == "o" ||
-  #      @cases[2] == "o" && @cases[4] == "o" && @cases[6] == "o"
-  #     puts "Y a gagné"
-  #     else 
-  #       puts "Match nul"
-  #     end
-      
-  
-
-  def addPlayer(name,symbol)       # définit le joueur et son symbole
-    player = Player.new(name,symbol)
-    return player
-end
-
 end
 game = Game.new("")
 game.action
-# game.game_ending
+
